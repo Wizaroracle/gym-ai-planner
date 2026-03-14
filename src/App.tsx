76aee293620ac2aecbdf0ae981b382e1babd2d69
Christian Jonel Cañanes <christianjonelcananes@gmail.com>
2026-03-14 17:28:38 +0800

    implement Navbar components

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import Onboarding from "./pages/Onboarding";
 import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
 import Account from "./pages/Account";
+import NavBar from "./components/layout/NavBar";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/auth/:pathname" element={<Auth />} />
-        <Route path="/account/:pathname" element={<Account />} />
-      </Routes>
+    <div className="min-h-screen flex flex-col">
+      <NavBar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/auth/:pathname" element={<Auth />} />
+          <Route path="/account/:pathname" element={<Account />} />
+        </Routes>
+      </main>
     </div>
   );
 }
