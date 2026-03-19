9e2b13397670f707665b6f4e9781fd79a6548f4b
Christian Jonel Cañanes <christianjonelcananes@gmail.com>
2026-03-20 01:22:54 +0800

    fix vercel deployment issues

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Onboarding from "./pages/Onboarding";
 import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
-import Account from "./pages/Account";
 import NavBar from "./components/layout/NavBar";
 import { NeonAuthUIProvider } from "@neondatabase/neon-js/auth/react";
 import { authClient } from "./lib/auth";
@@ -19,7 +18,6 @@ function App() {
             <Route path="/onboarding" element={<Onboarding />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/auth/:pathname" element={<Auth />} />
-            <Route path="/account/:pathname" element={<Account />} />
           </Routes>
         </main>
       </div>
